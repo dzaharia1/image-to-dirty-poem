7a002d42a7bd781c91405cfa5a8ec48e01ac9f67
Dan Zaharia <zaharia.danny@gmail.com>
2026-02-12 22:12:17 -0500

    Ease rate limiting

--- middleware/security.js
+++ middleware/security.js
@@ -21,8 +21,8 @@ export const configureSecurity = (app) => {
   }));
 
   const limiter = rateLimit({
-    windowMs: 5 * 60 * 1000, // 5 minutes
-    max: 100, // Limit each IP to 100 requests per windowMs
+    windowMs: 1 * 60 * 1000, // 1 minute
+    max: 100, // Limit each IP to 100 requests per 1 minute
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   });
